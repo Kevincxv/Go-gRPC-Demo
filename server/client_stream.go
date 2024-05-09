b297b3f90c4f36f187992a5beeb97a9988368316
Kevin Camacho <Kevincxv@outlook.com>
2024-05-09 09:50:20 -0400

    Paramter Update

--- server/client_stream.go
+++ server/client_stream.go
@@ -6,7 +6,7 @@ import (
 	pb "github.com/Kevincxv/Go-gRPC-Demo/proto"
 )
 
-func (s *helloServer) SayHelloClientStreaming(req *pb.NamesList, stream pb.GreetService_SayHelloClientStreamingServer) error {
+func (s *helloServer) SayHelloClientStreaming(stream pb.GreetService_SayHelloClientStreamingServer) error {
 	var messages []string
 	for {
 		req, err := stream.Recv()
